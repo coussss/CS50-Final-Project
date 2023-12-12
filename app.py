8128cdffd986e316cdb500355ce54ea12c3d7b46
coussss <adamjbox@gmail.com>
2023-12-12 15:15:50 +0000

    Created .csv for possible outcomes and tested on index.html

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, url_for
+import csv
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    with open("blackjack-hard-totals.csv", 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        rows = list(reader)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        cards_dict=[]
+
+        for i in range(11):
+            card = rows[0][i]
+            cards_dict.append(card)
+
+    
+    return render_template("index.html", cards_dict = cards_dict)
