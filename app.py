91eddfbcf693ab106189866f6be57156d9b2055a
coussss <adamjbox@gmail.com>
2023-12-29 18:07:58 +0000

    created soft total csv and split total csv finished core application ready for css

--- app.py
+++ app.py
@@ -3,6 +3,27 @@ import csv
 
 app = Flask(__name__)
 
+def softTotal(dealer_total, player_total):
+    with open("blackjack-soft-totals.csv", 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        rows = list(reader)
+
+        return(rows[(player_total-12)][(dealer_total-1)])
+    
+def splitTotal(dealer_total, player_total):
+    with open("blackjack-split-totals.csv", 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        rows = list(reader)
+
+        return(rows[(player_total-3)][(dealer_total-1)])
+    
+def hardTotal(dealer_total, player_total):
+    with open("blackjack-hard-totals.csv", 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        rows = list(reader)
+
+        return(rows[(player_total-1)][(dealer_total-1)])
+
 @app.route("/")
 def home():
     with open("blackjack-hard-totals.csv", 'r') as csv_file:
@@ -21,6 +42,13 @@ def home():
 @app.route("/generate", methods=["GET", "POST"])
 def generate():
 
+    dealer_card = int(request.form.get("dealer-card"))
+    player_card_1 = int(request.form.get("player-card-1"))
+    player_card_2 = int(request.form.get("player-card-2"))
+
+    player_decision = None
+
+    
     with open("blackjack-hard-totals.csv", 'r') as csv_file:
         reader = csv.reader(csv_file)
         rows = list(reader)
@@ -31,18 +59,23 @@ def generate():
             card = rows[0][i]
             cards_dict.append(card)
 
-    dealer_card = request.form.get("dealer-card")
-    player_card_1 = request.form.get("player-card-1")
-    player_card_2 = request.form.get("player-card-2")
+    if player_card_1 == 11 or player_card_2 == 11:
+        player_soft_total = player_card_1 + player_card_2
 
-    if dealer_card == "A":
-        dealer_card = 11
-    if player_card_1 == "A":
-        player_card_1 = 11
-    if player_card_2 == "A":
-        player_card_2 = 11
+        player_decision = softTotal(dealer_card, player_soft_total)
 
-    dealer = int(dealer_card)
-    player = int(player_card_1) + int(player_card_2)
+        return render_template("index.html", cards_dict = cards_dict, test = player_decision) 
 
-    return render_template("index.html", cards_dict = cards_dict, test = rows[(player-1)][(dealer-1)])
+    elif player_card_1 == player_card_2:
+        player_split_total = player_card_1 + player_card_2
+
+        player_decision = splitTotal(dealer_card, player_split_total)
+
+        return render_template("index.html", cards_dict = cards_dict, test = player_decision) 
+    
+    else:
+        player_hard_total = player_card_1 + player_card_2
+
+        player_decision = hardTotal(dealer_card, player_hard_total)
+
+        return render_template("index.html", cards_dict = cards_dict, test = player_decision) 
